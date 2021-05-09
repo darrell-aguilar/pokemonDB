f67025b40fa8b14511ce296131f086807bec1500
Dah <dah@mbp-16.broadband>
2021-05-09 15:05:01 +0100

    initial setup for web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      pokemons: [],
+      search: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+    componentDidMount() {
+    const url = "https://pokeapi.co/api/v2/pokemon/?limit=100"
+    fetch(url)
+    .then(response => response.json())
+    .then(result => this.setState({
+    pokemons: result.results
+    }))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  render() {
+    const filterPokemon = this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.search));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <FeatureAndSearch changed={this.handleChange}/>
+      <Items pokemon={filterPokemon} />
     </div>
-  );
+  )};
+}
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="navBar">
+          <ul className="navItems">
+            <li><a href="#/">Home</a></li>
+            <li><a href="#/">Pokemon</a></li>
+            <li><a href="#/">Abilities</a></li>
+            <li><a href="#/">About</a></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class FeatureAndSearch extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="main">
+          <div className="feature">
+          </div>
+          <div className="searchBar">
+            <input className="pokemonSearch" placeholder="Type in PoKemon name to Filter" type="search" onChange={this.props.changed}></input>
+          </div>
+        </div>
+       
+      </div>
+    )
+  }
+}
+
+
+class Items extends React.Component {
+  render() {
+    const pokemonListProps = this.props.pokemon.map(x => <div className="pokemonCard" key={x.name}>{x.name}</div>);
+    return (
+      <div>
+         <div className="pokemonList">{pokemonListProps}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
