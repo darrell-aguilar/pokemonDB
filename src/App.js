b89a38b471a4267168d2a61704ea4028fddb401d
Dah <dah@mbp-16.broadband>
2021-05-09 21:19:30 +0100

    Added images and fixed format on the main page

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends React.Component {
     componentDidMount() {
     const url = "https://pokeapi.co/api/v2/pokemon/?limit=100"
     fetch(url)
-    .then(response => response.json())
-    .then(result => this.setState({
-    pokemons: result.results
+    .then(res => res.json())
+    .then(response => this.setState({
+    pokemons: response.results
     }))
   }
 
@@ -26,12 +26,12 @@ class App extends React.Component {
   }
 
   render() {
-    const filterPokemon = this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.search));
+    const filterPokemon = this.state.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.state.search.toLowerCase()));
   return (
     <div className="App">
       <Nav />
       <FeatureAndSearch changed={this.handleChange}/>
-      <Items pokemon={filterPokemon} />
+      <PokemonGroup pokemon={filterPokemon} />
     </div>
   )};
 }
@@ -72,15 +72,23 @@ class FeatureAndSearch extends React.Component {
 }
 
 
-class Items extends React.Component {
+class PokemonGroup extends React.Component {
   render() {
-    const pokemonListProps = this.props.pokemon.map(x => <div className="pokemonCard" key={x.name}>{x.name}</div>);
     return (
       <div>
-         <div className="pokemonList">{pokemonListProps}</div>
+         <div className="pokemonList">{this.props.pokemon.map(pokemonChar => (<PokemonSingle key={pokemonChar.name} pokemonName={pokemonChar.name}></PokemonSingle>))}</div>
       </div>
     )
   }
 }
 
+
+class PokemonSingle extends React.Component {
+  render() {
+    return (
+      <div className="pokemonName"><img alt={`${this.props.pokemonName}`} title={`${this.props.pokemonName}`} src={`https://img.pokemondb.net/artwork/${this.props.pokemonName}.jpg`}/><br/><div className="pokemonTitle">{this.props.pokemonName.charAt(0).toUpperCase() + this.props.pokemonName.slice(1)}</div>
+      </div>
+    )
+  }
+}
 export default App;
