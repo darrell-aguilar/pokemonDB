d47a327efbd3018f76d06d0786e174ab35d159c3
darrell404 <darrell029@gmail.com>
2024-06-16 21:35:52 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { createStore } from "redux";
-import allReducers from "./reducers/RootReducer";
-import { Provider } from "react-redux";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { createStore } from "redux"
+import allReducers from "./reducers/RootReducer"
+import { Provider } from "react-redux"
 
 const store = createStore(
-  allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,4 +17,4 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
-);
+)
